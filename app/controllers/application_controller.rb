c33f74f38087186ca00cb44bb8808fcc025149b8
jreyes8631 <jreyes8631@gmail.com>
2021-01-20 19:36:00 -0500

    mock current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::API
-    before_action :authenticate_user!
+    #before_action :authenticate_user!
+
+    def current_user
+        User.first
+    end
 end
